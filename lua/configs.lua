bb7839987e075a16da61b647a29e8e0f1e50b095
Eron Alves <eron.alves@rocketmail.com>
2023-07-09 23:47:10 -0300

    update configs

--- lua/configs.lua
+++ lua/configs.lua
@@ -4,13 +4,36 @@ vim.cmd([[ set autoindent expandtab tabstop=2 shiftwidth=2 ]])
 -- linenumber
 vim.cmd([[ set number ]])
 
+-- column number
+vim.cmd([[ set cc=80 ]])
+
 -- colorscheme
 
 vim.cmd([[ set termguicolors ]])
-vim.cmd([[ colorscheme SerialExperimentsLain ]])
+vim.cmd([[ colorscheme molokai  ]])
 
 -- terminal
-vim.api.nvim_create_user_command("Term", "split | wincmd j | resize 10 | terminal", {})
+vim.api.nvim_create_user_command("Term",
+  function(options)
+    local args = options.args
+    if not (args == '') then
+      vim.cmd("wincmd w")
+    end
+    vim.cmd("split")
+    vim.cmd("wincmd j")
+    vim.cmd("resize 10")
+
+    if args == '' then
+      vim.cmd("terminal")
+      return
+    end
+
+    vim.cmd("terminal zsh -c \"cd " .. args .. ";zsh\"")
+  end, { nargs = '?' })
+-- tabs
+vim.keymap.set("n", "<space>t", ":tab new<CR>")
+vim.keymap.set("n", "tp", ":tabprevious<CR>")
+vim.keymap.set("n", "tn", ":tabnext<CR>")
 
 -- lualine
 local status, lualine = pcall(require, "lualine")
@@ -18,7 +41,7 @@ if (not status) then return end
 
 lualine.setup {
   options = {
-    theme = 'solarized_dark'
+    theme = 'auto'
   }
 }
 -- autotag and autopair
@@ -54,6 +77,15 @@ telescope.setup {
       },
     },
   },
+  pickers = {
+    buffers = {
+      mappings = {
+        n = {
+          ["d"] = actions.delete_buffer
+        }
+      }
+    }
+  }
 }
 
 vim.keymap.set("n", "tt", ":Telescope<CR>")
@@ -80,6 +112,9 @@ vim.keymap.set('n', ';;', function()
 end)
 vim.keymap.set('n', ';e', function()
   builtin.diagnostics()
+end)
+vim.keymap.set("n", ";b", function()
+  builtin.buffers()
 end)
 
 -- telescope browser
@@ -128,19 +163,6 @@ vim.keymap.set("n", "sf", function()
   })
 end)
 
--- bufferline
-require('bufferline').setup({
-  options = {
-    themable = true,
-    numbers = "buffer_id",
-    diagnostics = "coc",
-    show_buffer_icons = true
-  }
-})
-
-vim.keymap.set('n', 'bn', '<Cmd>BufferLineCycleNext<CR>', {})
-vim.keymap.set('n', 'bp', '<Cmd>BufferLineCyclePrev<CR>', {})
-
 -- gitsigns
 require('gitsigns').setup {
 }
@@ -168,59 +190,107 @@ require("nvim-tree").setup({
 
 vim.keymap.set('n', 'nt', ':NvimTreeToggle<CR>')
 
---lsp-zero
-local lsp = require('lsp-zero').preset({})
+--lsp
 
-lsp.on_attach(function(client, bufnr)
-  lsp.default_keymaps({ buffer = bufnr })
+local lspconfig = require('lspconfig')
+local protocol = require('vim.lsp.protocol')
 
-  vim.keymap.set('n', "<space>ca", function()
-    vim.lsp.buf.code_action()
-  end)
-  vim.keymap.set('v', "<space>ca", function()
-    vim.lsp.buf.code_action()
-  end)
-end)
+-- format on save
+local on_attach = function(client, bufnr)
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', "v:lua.vim.lsp.omnifunc")
+  if client.supports_method("textDocument/formatting") then
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      group = vim.api.nvim_create_augroup('Format', { clear = true }),
+      buffer = bufnr,
+      callback = function()
+        vim.lsp.buf.format()
+      end
+    })
+  end
+  local bufopts = { noremap = true, silent = true, buffer = bufnr }
+  local opts = { noremap = true, silent = true }
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+  vim.keymap.set('n', '<space>K', vim.lsp.buf.signature_help, bufopts)
+  vim.keymap.set('n', 'gt', vim.lsp.buf.type_definition, bufopts)
+  vim.keymap.set('n', '<F2>', vim.lsp.buf.rename, bufopts)
+  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+  vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+  vim.keymap.set('n', '<space>f', vim.lsp.buf.format, bufopts)
+  vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
+  vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+  vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+end
 
-lsp.setup_servers({ 'lua_ls', "tsserver", "cssls", "html", "tailwindcss" })
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
+capabilities.workspace = {}
+capabilities.workspace.didChangeWatchedFiles = {}
+capabilities.workspace.didChangeWatchedFiles.dynamicRegistration = true
 
--- (Optional) Configure lua language server for neovim
-require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+local language_servers = { 'tsserver', 'tailwindcss', 'cssls', 'html', 'phpactor', 'volar' }
 
-lsp.setup()
+for key, value in pairs(language_servers) do
+  lspconfig[value].setup {
+    on_attach = on_attach,
+    capabilities = capabilities
+  }
+end
 
 
-local cmp = require('cmp')
-local cmp_action = require('lsp-zero').cmp_action()
+lspconfig.lua_ls.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { 'vim' },
+      },
+      workspace = {
+        library = vim.api.nvim_get_runtime_file("", true),
+        checkThirdParty = false
+      }
+    }
+  }
+}
+
+local cmp = require 'cmp';
+local lspkind = require 'lspkind'
 
 cmp.setup({
-  mapping = {
-    ['<CR>'] = cmp.mapping.confirm({ select = false }),
-    ["<C-Space>"] = cmp.mapping.complete(),
-    ["<TAB>"] = function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      else
-        fallback()
-      end
-    end,
-    ["<S-TAB>"] = function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      else
-        fallback()
-      end
+  snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body)
     end
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['C-Space'] = cmp.mapping.complete(),
+    ['<CR>'] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
+  }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'buffer' },
+  }),
+  formatting = {
+    format = lspkind.cmp_format({
+      mode = 'symbol',       -- show only symbol annotations
+      maxwidth = 50,         -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+      ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+
+      -- The function below will be called before any actual modifications from lspkind
+      -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+      before = function(entry, vim_item)
+        return vim_item
+      end
+    })
   }
 })
 
--- format on save
-vim.api.nvim_create_autocmd("BufWritePre", {
-  buffer = buffer,
-  callback = function()
-    vim.lsp.buf.format { async = false }
-  end
-})
+vim.cmd [[ set completeopt=menuone,noinsert,noselect
+highlight! default link CmpItemKind CmpItemMenuDefault
+]]
 
 -- prettier
 local null_ls = require("null-ls")
@@ -257,6 +327,18 @@ null_ls.setup({
   sources = {
     null_ls.builtins.diagnostics.eslint_d,
     null_ls.builtins.code_actions.eslint_d,
-    null_ls.builtins.formatting.prettierd
+    null_ls.builtins.formatting.prettierd.with({
+      extra_filetypes = { "php" }
+    }),
+    null_ls.builtins.diagnostics.phpcs.with({
+      extra_args = { "--standard=PSR12" }
+    })
   }
 })
+
+-- treesitter
+require 'nvim-treesitter.configs'.setup {
+  highlight = {
+    enable = true
+  }
+}
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -20,7 +20,6 @@ packer.startup(function(use)
     requires = { { 'nvim-lua/plenary.nvim' } }
   }
   use 'nvim-telescope/telescope-file-browser.nvim'
-  use 'akinsho/nvim-bufferline.lua'
   use 'lewis6991/gitsigns.nvim'
   use 'dinhhuy258/git.nvim'
   use {
@@ -34,19 +33,15 @@ packer.startup(function(use)
   }
   use 'nvim-tree/nvim-tree.lua'
   use 'jparise/vim-graphql'
-  use {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v2.x',
-    requires = {
-      -- LSP Support
-      { 'neovim/nvim-lspconfig' }, -- Required
-      -- Autocompletion
-      { 'hrsh7th/nvim-cmp' },      -- Required
-      { 'hrsh7th/cmp-nvim-lsp' },  -- Required
-      { 'L3MON4D3/LuaSnip' },      -- Required
-    }
-  }
-
+  use 'neovim/nvim-lspconfig'
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'hrsh7th/nvim-cmp'
+  use 'hrsh7th/cmp-buffer'
+  use 'L3MON4D3/LuaSnip'
+  use 'onsails/lspkind.nvim'
   use('jose-elias-alvarez/null-ls.nvim')
   use('MunifTanjim/prettier.nvim')
+  use 'tpope/vim-fugitive'
+  use 'mattn/emmet-vim'
+  use 'RRethy/nvim-base16'
 end)
